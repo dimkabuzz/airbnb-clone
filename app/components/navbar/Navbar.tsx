7d50804d21a6a69d7f52da1d9a18d3bcff7b1f72
dimkabuzz <dimkabuzz@gmail.com>
2023-11-21 20:36:14 +0100

    Categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Container from "@/app/components/Container";
 import Logo from "@/app/components/navbar/Logo";
 import Search from "@/app/components/navbar/Search";
 import UserMenu from "@/app/components/navbar/UserMenu";
+import Categories from "@/app/components/navbar/Categories";
 
 type NavbarProps = {
   currentUser?: User | null;
@@ -22,6 +23,8 @@ function Navbar({ currentUser }: NavbarProps) {
           </div>
         </Container>
       </div>
+
+      <Categories />
     </div>
   );
 }
